fa34c795608cd3519027f39804e3063271284b6f
Marjan <marjan@tie-international.com>
2023-11-15 17:12:15 +0100

    new exercises

--- Algorithmen/Algorithm Challenges/AB04/displaying_parametes.py
+++ Algorithmen/Algorithm Challenges/AB04/displaying_parametes.py
@@ -1,11 +1,10 @@
 def displaying_parameters(**kwargs):
     list_elements = []
     for key, value in kwargs.items():
-        print(type(value).__name__)
-        key_value_pair = []
-        key_value_pair.append([key, value])
+        type_name = type(value).__name__
+        key_value_pair = [type_name, value]
         list_elements.append(key_value_pair)
     return list_elements
 
 
-displaying_parameters()
+print(displaying_parameters(arg1="Hi", arg2=True, arg3=42, arg4=0.6))
